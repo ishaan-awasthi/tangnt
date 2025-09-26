84d2c2551fee78e2191ae140134f75c8d3274dc1
ishaan-awasthi <ishaanawasthi05@gmail.com>
2025-09-26 13:54:33 -0700

    refactor incoming

--- tangnt-app/src/components/ThemeProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useTheme } from "@/hooks/useTheme";
-
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  useTheme(); // Initialize theme
-
-  return <>{children}</>;
-} 
